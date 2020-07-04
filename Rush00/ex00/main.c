9d37f4b3cae5cf1bc0a21d020b92bd7a16e3b685
Jiho Lee <jiholee@c3r6s3.42seoul.kr>
2020-07-04 11:31:32 +0900

    test

---
+++ Rush00/ex00/main.c
@@ -0,0 +1,7 @@
+#include <unistd>
+
+int main()
+{
+	rush(5,5);
+	return (0);
+}
---
+++ Rush00/ex00/test.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+int		rush(int x, int y);
+
+int rush(int x, int y)
+{
+	int i=0, j=0;
+	
+	while(j < y)
+	{
+		i = 0;
+		while(i < x)
+		{
+			if( (i == 0 | i == x-1) && ( j == 0 | j == x-1) )
+			{
+				write(1, "o", 1);
+				i++;
+			}
+
+
+			else if( (j == 0  | j == y-1)   && ( i != 0 | i != x-1 ))
+			{
+				write(1, "-", 1); 
+				i++;
+			}
+
+			else if( (j != 0 | j != y-1) && ( i == 0 | i == x-1) )
+			{
+				write(1, "|", 1);
+				i++;			
+			}
+
+
+			else
+			{ 
+				write(1, " ", 1);
+				i++;
+			}
+
+		}
+
+		j++;
+		write(1, "\n", 1);	
+	}
+
+	return (0);
+}
+
+int main()
+{
+	rush(4,4);
+	return (0);
+}
