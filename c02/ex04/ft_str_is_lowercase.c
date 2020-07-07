ff942fe3054573c618114433985360944fc5687e
Jiho Lee <jiholee@c3r6s3.42seoul.kr>
2020-07-07 13:30:15 +0900

    ft_str_is_lowercase

--- c02/ex04/ft_str_is_lowercase.c
+++ c02/ex04/ft_str_is_lowercase.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
- //////////
+
 int		ft_str_is_lowercase(char *str)
 {
+	int i;
+	int res;
+
+	i = 0;
+	res = 0;
+
 	if(*str == '\0')
 		return 1;
 
-	while(*str++)
+	while(str[i])
 	{
-		if (*str <= 97 && *str >= 122)  // exclud a ~ z 
-		{
-			break;
-			return 0;
+		if (str[i] >= 97 && str[i] <= 122)  //a ~ z 
+		{	
+			res++;
+			i++;
 		}
 		else
-			return 0;
+			i++;
 	}
-	return 1;
+	
+	if(res == i)
+		return 1;
+	else 
+		return 0;
 }
 
-int main(void)
+int 	main(void)
 {
 	char *str1 = "abcd"; // 1
 	char *str2 = "Abcd"; // 0
