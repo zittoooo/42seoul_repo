24d052fc292c1d3211c970363a8d437ca96d34f3
Jiho Lee <jiholee@c3r6s3.42seoul.kr>
2020-07-07 11:48:25 +0900

    +null

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdio.h>
-
+#include <string.h>
 char	*ft_strcpy(char *dest, char *src)
 {
 	int i;
@@ -11,18 +11,26 @@ char	*ft_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
-
+		
+	dest[i] = '\0';
 	return dest;
 }
 
 int main()
 {
-	char s1[] = "abcd";
-	char s2[4] = "";
+	char s1[5] = "abcd";
+	char s2[5] = "";
 
 	ft_strcpy(s2, s1);
 
-	printf("%s\n", s2);
+	printf("ft_strcpy : %s\n", s2);
+
+	char *s3 = "abcd";
+	char *s4= "";
+	strcpy(s4, s3);
+	printf("strcpy : %s\n", s4);
+
+
 	return 0;
 }
 
