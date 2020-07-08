6cae4f2711a306c47cb86986dd282dc484c3a6da
zittoooo <stellar7484@naver.com>
2020-07-08 11:57:07 +0900

    help

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: jiholee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 17:46:42 by jiholee           #+#    #+#             */
-/*   Updated: 2020/07/06 17:47:24 by jiholee          ###   ########.fr       */
+/*   Updated: 2020/07/08 11:48:00 by jiholee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_rev_int_tab(int *tab, int size)
 	int j;
 
 	i = 0;
-	j = sizeo - 1;
+	j = size - 1;
 	while (i < size)
 	{
 		tmp[i] = tab[j];
@@ -27,11 +27,9 @@ void	ft_rev_int_tab(int *tab, int size)
 		j--;
 	}
 	i = 0;
-	j = size - 1;
 	while (i < size)
 	{
-		tab[i] = tmp[j];
+		tab[i] = tmp[i];
 		i++;
-		j--;
 	}
 }
